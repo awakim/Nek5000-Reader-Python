26574a92ee808565b3c55d0d7f02ad2cffde493d
awakim <50945323+awakim@users.noreply.github.com>
2019-07-02 13:58:31 +0200

    Add files via upload

--- contour_plot_example.py
+++ contour_plot_example.py
@@ -9,24 +9,30 @@ header=nr.readheader(fname)
 elmap=nr.readelmap(fname,header)
 geo=nr.readgeo(fname,header,elmap)
 vel=nr.readvel(fname,header,elmap)
-pres=nr.readpres(fname,header,elmap)
+pre=nr.readpres(fname,header,elmap)
 te= time.time()
 print "time elapsed for full file :: ", te-ts
 
 
 
-X=[]
-Y=[]
-Z=[]
+X=np.array([])
+Y=np.array([])
+Z=np.array([])
 
-for i in range(222):
-    for j in range(111):
-        for iy in range(6):
-            X+=list(geo[i+222*j][0,0,iy,:])
-            Y+=list(geo[i+222*j][1,0,iy,:])
-            Z+=list(pres[i+222*j][0,0,iy,:])
+# for i in range(111):
+#     for j in range(111):
+#         for iy in range(6):
+#             X+=list(geo[111+i+222*j][0,0,iy,:])
+#             Y+=list(geo[111+i+222*j][1,0,iy,:])
+#             Z+=list(pres[111+i+222*j][0,0,iy,:])
 
+for i in range(60):
+    for j in range(60):
+        X=np.append(X,geo[111+i+222*j][0,0,:,:])
+        Y=np.append(Y,geo[111+i+222*j][1,0,:,:])
+        Z=np.append(Z,pre[111+i+222*j][0,0,:,:])
 
+print X,len(X)
 
 v=np.linspace(-1.,1.,16)
 plt.tricontourf(X,Y,Z,v,cmap=plt.cm.bwr,extend='both')
